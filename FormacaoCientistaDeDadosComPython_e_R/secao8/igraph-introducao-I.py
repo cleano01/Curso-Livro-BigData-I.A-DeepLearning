7513ff5e442b37aec8f2dbfb5dbb15d660f9cba6
Cleano Ferreira <cleano.ferreira@dce.ufpb.br>
2020-03-13 21:20:23 -0300

    (secao8)
    1. instalar os pacotes da aula;
    
    
    
    2. instalar um pacote de exibição de imagem para o grafo, nesse meu caso foi o eog;
    
    
    
    2.1 De o comando em seu terminal  sudo apt-get update;
    
    
    
    2.2 De o comando em seu terminal  sudo apt-get install eog;
    
    
    
    3. Para exibir o grafo em imagem insira o codigo abixo antes de exibir (plot) do grafo:
    
    >>> from igraph import Configuration
    
    >>> cfg = Configuration.instance()
    
    >>> cfg["apps.eog"] = "start"
    
    
    
    4. Com a configuração em cima feita, podemos fazer um teste para exibir o grafo
    
    >>> from igraph import *
    
    >>> g = Graph.Famous("petersen")
    
    >>> plot(g)
    
    
    
    LINKS:
    
    
    
    https://igraph-help.nongnu.narkive.com/Nvqtsy3h/error-when-plotting-igraph
    
    https://stackoverflow.com/questions/30559041/is-it-possible-to-plot-igraph-plots-on-cygwin

---
+++ FormacaoCientistaDeDadosComPython_e_R/secao8/igraph-introducao-I.py
@@ -0,0 +1,56 @@
+from igraph import Configuration
+from igraph import Graph
+from igraph import plot
+#import cairo
+
+#configuracao para exebir o grafo
+cfg = Configuration.instance()
+cfg["apps.eog"] = "start"
+
+#edges = arestas
+#direct = TRUE  estamos informando que o grafo sera direcionado
+grafo1 = Graph(edges = [(0,1), (1,2), (2,3), (3,0)],
+             directed = True)
+
+#vcount() = informa quantos vertices temos
+grafo1.vs['label'] = range(grafo1.vcount())
+#print(grafo1)
+#plot(grafo1, bbox= (300, 300))
+
+
+
+
+#edges = arestas
+#direct = TRUE  estamos informando que o grafo sera direcionado
+grafo2 = Graph(edges = [(0,1),(1,2),(2,3),(3,0),(0,3),
+              (3,2),(2,1),(1,0)], 
+              directed = True)
+
+#vcount() = informa quantos vertices temos
+grafo2.vs['label'] = range(grafo2.vcount())
+#print(grafo2)
+#plot(grafo2, bbox= (300, 300))
+
+
+#edges = arestas
+#direct = TRUE  estamos informando que o grafo sera direcionado
+grafo3 = Graph(edges = [(0,1), (1,2), (2,3), (3,0), (1,1)],
+             directed = True)
+
+#vcount() = informa quantos vertices temos
+grafo3.vs['label'] = range(grafo3.vcount())
+#print(grafo3)
+#plot(grafo3, bbox= (300, 300))
+
+
+#edges = arestas
+#direct = TRUE  estamos informando que o grafo sera direcionado
+grafo4 = Graph(edges = [(0,1), (1,2), (2,3), (3,0), (1,1)],
+             directed = True)
+
+grafo4.add_vertex(5)
+
+#vcount() = informa quantos vertices temos
+grafo4.vs['label'] = range(grafo4.vcount())
+print(grafo4)
+plot(grafo4, bbox= (300, 300))
